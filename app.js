29132374465ca6d01ec5ab3da3510e2d6af4bbe1
EvgeniiyaR <tulpansik@mail.ru>
2023-06-17 00:00:06 +0300

    feat: add error for page not found

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
+  res.status(404).send({ message: 'Not found' });
+
+  next();
+});
+
+app.use((req, res, next) => {
   req.user = {
     _id: '648c9284c2df3b4964e875c8', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
