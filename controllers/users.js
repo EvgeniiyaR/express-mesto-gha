2360bc2c593164cac304733254ab5ca2473a6473
EvgeniiyaR <tulpansik@mail.ru>
2023-06-16 22:57:38 +0300

    feat: add for users errors catch

--- controllers/users.js
+++ controllers/users.js
@@ -3,35 +3,79 @@ const User = require('../models/user');
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'Server Error' }));
 };
 
 const getUser = (req, res) => {
   const { id } = req.params;
   User.findById(id)
-    .then((user) => res.status(200).send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'User not found' });
+      }
+      return res.status(200).send(user);
+    })
+    .catch(() => res.status(500).send({ message: 'Server Error' }));
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: `${err.errors.name.properties.message}` });
+      }
+      return res.status(500).send({ message: 'Server Error' });
+    });
 };
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.status(201).send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Server Error' });
+      }
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: `${err.errors.name.properties.message}` });
+      }
+      return res.status(500).send({ message: 'Server Error' });
+    });
 };
 
 const updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((user) => res.status(201).send(user))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'User not found' });
+      }
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: `${err.errors.name.properties.message}` });
+      }
+      return res.status(500).send({ message: 'Server Error' });
+    });
 };
 
 module.exports = {
