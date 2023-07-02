52f79d2ce7c92be14138d59045036e94b0efaa9e
EvgeniiyaR <tulpansik@mail.ru>
2023-07-02 13:56:34 +0300

    fix: joi validation for /signup avatar

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const auth = require('../middlewares/auth');
 router.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    avatar: Joi.string().required().domain(),
+    avatar: Joi.string().required().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
     password: Joi.string().required().min(8),
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().min(2).max(30),
