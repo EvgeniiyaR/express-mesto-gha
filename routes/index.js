f0b04f3802a58d3c52b634284e86186e03757143
EvgeniiyaR <tulpansik@mail.ru>
2023-07-02 14:00:05 +0300

    fix: joi validation for /signup optional fields

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().min(2).max(30),
-  }),
+  }).unknown(true),
 }), createUser);
 
 router.use(errors());
